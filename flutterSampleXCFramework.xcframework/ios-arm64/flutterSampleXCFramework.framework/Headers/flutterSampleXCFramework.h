ae214c139b16352821e788cf8c7b551dbfdbf722
Boomibalan <boomibalan.b@syncfusion.com>
2025-04-08 12:18:55 +0530

    Changes

---
+++ flutterSampleXCFramework.xcframework/ios-arm64/flutterSampleXCFramework.framework/Headers/flutterSampleXCFramework.h
@@ -0,0 +1,18 @@
+//
+//  flutterSampleXCFramework.h
+//  flutterSampleXCFramework
+//
+//  Created by mohammedismailsameer on 08/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for flutterSampleXCFramework.
+FOUNDATION_EXPORT double flutterSampleXCFrameworkVersionNumber;
+
+//! Project version string for flutterSampleXCFramework.
+FOUNDATION_EXPORT const unsigned char flutterSampleXCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <flutterSampleXCFramework/PublicHeader.h>
+
+
